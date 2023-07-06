c08251c646b67fd33e37b2943ed973357cb8db83
caoguosheng <caoguosheng.smg.cn>
2023-07-06 16:04:08 +0800

    first commit

--- Example/GSPodTest/ViewController.swift
+++ Example/GSPodTest/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import GSPodTest
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let p = Person()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ GSPodTest/Classes/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  GSPodTest
+//
+//  Created by 曹国盛 on 2023/7/6.
+//
+
+import Foundation
+
+class Person{
+    var name = ""
+}
